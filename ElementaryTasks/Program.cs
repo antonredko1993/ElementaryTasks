3a06edfa6a707e4dd580f3580a6c1e56ea43023b
ared <ared@LT-905410>
2019-10-21 14:57:23 +0300

    Created project

---
+++ ElementaryTasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElementaryTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
