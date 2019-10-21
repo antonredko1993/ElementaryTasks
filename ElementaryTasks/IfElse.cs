8d9b6f414b62bfa6ba087a5d7634076479e1ab70
ared <ared@LT-905410>
2019-10-21 16:36:39 +0300

    FindMax

--- ElementaryTasks/IfElse.cs
+++ ElementaryTasks/IfElse.cs
@@ -8,13 +8,39 @@ namespace ElementaryTasks
     {
         public void FindMax()
         {
+            Console.WriteLine("Enter the first value");
+            int maxValue;
+            if (int.TryParse(Console.ReadLine(), out maxValue))
+            {
+            Console.WriteLine("Enter the second value");
+            }
 
+            int userInput2;
+            if (int.TryParse(Console.ReadLine(), out userInput2))
+            {
+                Console.WriteLine("Enter the trird value");
+            }
+
+            int userInput3;
+            if (int.TryParse(Console.ReadLine(), out userInput3))
+
+
+            if (userInput2 > maxValue)
+            {
+                    maxValue = userInput2;
+            }
+            if (userInput3 > maxValue)
+            {
+                maxValue = userInput3;
+            }
+
+            Console.WriteLine($"Value {maxValue} is the biggest");
         }
 
-        public void EvenNmbers()
+        /*public void EvenNmbers()
         {
 
-        }
+        }*/
 
     }
 }
